ec52811ee07a45ca4b09eea7de782199cfde0cf8
KKTRKKT <paulhana6006@gmail.com>
2023-03-30 22:44:15 +0900

    Update Client.java

--- src/main/java/me/kktrkkt/designpattern/decorator/Client.java
+++ src/main/java/me/kktrkkt/designpattern/decorator/Client.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Client {
 
-    private CommentService commentService;
+    private final CommentService commentService;
 
     public Client(CommentService commentService) {
         this.commentService = commentService;
@@ -17,12 +17,4 @@ public class Client {
     public List<String> readCommentList() {
         return commentService.getCommentList();
     }
-
-    public static void main(String[] args) {
-        Client client = new Client(new SpamFilteringCommentService());
-        client.writeComment("오징어게임");
-        client.writeComment("보는게 하는거 보다 재밌을 수가 없지...");
-        client.writeComment("http://whiteship.me");
-    }
-
 }
