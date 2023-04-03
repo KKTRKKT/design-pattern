811eee62dca68bea6201b9001ee46f15ed16180e
KKTRKKT <paulhana6006@gmail.com>
2023-04-03 20:49:02 +0900

    facade 패턴 구현 추가

---
+++ src/main/java/me/kktrkkt/designpattern/facade/EmailFacade.java
@@ -0,0 +1,30 @@
+package me.kktrkkt.designpattern.facade;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class EmailFacade {
+    public static void send(String to, String from, String subject, String text, String host) {
+        Properties properties = System.getProperties();
+        properties.setProperty("mail.smtp.host", host);
+
+        Session session = Session.getDefaultInstance(properties);
+
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(from));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            message.setSubject(subject);
+            message.setText(text);
+
+            Transport.send(message);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+}
