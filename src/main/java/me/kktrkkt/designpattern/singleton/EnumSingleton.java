293d1ec42b0a2b55ac33200f4e336fa263d4aada
KKTRKKT <paulhana6006@gmail.com>
2023-04-09 22:19:51 +0900

    주석 설명 수정

--- src/main/java/me/kktrkkt/designpattern/singleton/EnumSingleton.java
+++ src/main/java/me/kktrkkt/designpattern/singleton/EnumSingleton.java
@@ -1,7 +1,7 @@
 package me.kktrkkt.designpattern.singleton;
 
 // 완전무결한 형태의 싱글톤
-// 멀티스레딩에 안전하며, 리플렉션이나 바이트코드 조작에서도 안전하다.
+// 멀티스레딩에 안전하며, 리플렉션을 이용한 객체 생성에 안전한 방법
 public enum EnumSingleton {
     INSTANCE;
 }
