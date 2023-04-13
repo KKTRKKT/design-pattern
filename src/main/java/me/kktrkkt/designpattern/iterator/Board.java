7faa9409e78bf9281f25241903d2cbbf33d97871
KKTRKKT <paulhana6006@gmail.com>
2023-04-13 16:57:20 +0900

    iterator 구현 예제 추가

--- src/main/java/me/kktrkkt/designpattern/iterator/Board.java
+++ src/main/java/me/kktrkkt/designpattern/iterator/Board.java
@@ -1,8 +1,6 @@
 package me.kktrkkt.designpattern.iterator;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Board {
 
@@ -18,5 +16,15 @@ public class Board {
 
     public void addPost(String content) {
         this.posts.add(new Post(content));
+    }
+
+    public Iterator<Post> getIterator() {
+        return posts.iterator();
+    }
+
+    public Iterator<Post> getRecentIterator() {
+        posts.sort((p1, p2) -> p2.getCreatedDateTime().compareTo(p1.getCreatedDateTime()));
+        return posts.iterator();
+
     }
 }
--- src/test/java/me/kktrkkt/designpattern/iterator/BoardTest.java
+++ src/test/java/me/kktrkkt/designpattern/iterator/BoardTest.java
@@ -3,6 +3,7 @@ package me.kktrkkt.designpattern.iterator;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,17 +17,10 @@ class BoardTest {
         board.addPost("선생님, 저랑 디자인 패턴 하나 학습하시겠습니까?");
         board.addPost("지금 이 자리에 계신 여러분들은 모두 디자인 패턴을 학습하고 계신 분들입니다.");
 
-        // TODO 들어간 순서대로 순회하기
-        List<Post> posts = board.getPosts();
-        for (int i = 0 ; i < posts.size() ; i++) {
-            Post post = posts.get(i);
-        }
-
         // TODO 가장 최신 글 먼저 순회하기
-        Collections.sort(posts, (p1, p2) -> p2.getCreatedDateTime().compareTo(p1.getCreatedDateTime()));
-        for (int i = 0 ; i < posts.size() ; i++) {
-            Post post = posts.get(i);
-            System.out.println(post.getTitle());
+        Iterator<Post> recentIterator = board.getRecentIterator();
+        while (recentIterator.hasNext()){
+            System.out.println(recentIterator.next().getTitle());
         }
     }
 
