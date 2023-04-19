ed7e039d3cf0c5b7c471dcd087739e494b8c5fa0
KKTRKKT <paulhana6006@gmail.com>
2023-04-19 23:00:58 +0900

    state 테스트케이스 추가

--- src/test/java/me/kktrkkt/designpattern/state/OnlineCourseTest.java
+++ src/test/java/me/kktrkkt/designpattern/state/OnlineCourseTest.java
@@ -7,22 +7,84 @@ import static org.junit.jupiter.api.Assertions.*;
 class OnlineCourseTest {
 
     @Test
-    public void onlineCourse_test(){
+    public void draft_add_review_test() {
         Student student = new Student("whiteship");
-        Student keesun = new Student("keesun");
         OnlineCourse onlineCourse = new OnlineCourse();
 
-        keesun.addPrivateCourse(onlineCourse);
+        assertThrows(UnsupportedOperationException.class, () -> onlineCourse.addReview("review", student));
+    }
+
+    @Test
+    public void draft_add_student_test() {
+        Student student = new Student("whiteship");
+        OnlineCourse onlineCourse = new OnlineCourse();
 
         onlineCourse.addStudent(student);
-        onlineCourse.changeState(new PrivateState(onlineCourse));
+        assertEquals(student, onlineCourse.getStudents().get(0));
+    }
 
-        onlineCourse.addStudent(keesun);
+    @Test
+    public void published_add_review_test() {
+        Student student = new Student("whiteship");
+        OnlineCourse onlineCourse = new OnlineCourse();
+        onlineCourse.changeState(new PublishedState(onlineCourse));
 
         onlineCourse.addReview("hello", student);
 
-        System.out.println(onlineCourse.getState());
-        System.out.println(onlineCourse.getStudents());
-        System.out.println(onlineCourse.getReviews());
+        assertEquals("hello", onlineCourse.getReviews().get(0));
     }
+
+    @Test
+    public void published_add_student_test() {
+        Student student = new Student("whiteship");
+        OnlineCourse onlineCourse = new OnlineCourse();
+        onlineCourse.changeState(new PublishedState(onlineCourse));
+
+        onlineCourse.addStudent(student);
+
+        assertEquals(student, onlineCourse.getStudents().get(0));
+    }
+
+    @Test
+    public void private_add_review_test() {
+        Student student = new Student("whiteship");
+        OnlineCourse onlineCourse = new OnlineCourse();
+        onlineCourse.changeState(new PrivateState(onlineCourse));
+
+        assertThrows(UnsupportedOperationException.class, () -> onlineCourse.addReview("hello", student));
+    }
+
+    @Test
+    public void private_add_student_test() {
+        Student student = new Student("whiteship");
+        OnlineCourse onlineCourse = new OnlineCourse();
+        onlineCourse.changeState(new PrivateState(onlineCourse));
+
+        assertThrows(UnsupportedOperationException.class, () -> onlineCourse.addStudent(student));
+    }
+
+    @Test
+    public void private_add_review_not_contain_student_test() {
+        Student student = new Student("whiteship");
+        OnlineCourse onlineCourse = new OnlineCourse();
+        onlineCourse.addStudent(student);
+        onlineCourse.changeState(new PrivateState(onlineCourse));
+
+        onlineCourse.addReview("hello", student);
+
+        assertEquals("hello", onlineCourse.getReviews().get(0));
+    }
+
+    @Test
+    public void private_add_student_private_pass_test() {
+        Student student = new Student("whiteship");
+        OnlineCourse onlineCourse = new OnlineCourse();
+        onlineCourse.changeState(new PrivateState(onlineCourse));
+
+        student.addPrivateCourse(onlineCourse);
+        onlineCourse.addStudent(student);
+
+        assertEquals(student, onlineCourse.getStudents().get(0));
+    }
+
 }
