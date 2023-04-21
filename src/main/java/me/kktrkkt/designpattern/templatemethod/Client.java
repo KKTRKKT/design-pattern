9156622ccfe667b5c544253e0a9728ec65d0df2d
KKTRKKT <paulhana6006@gmail.com>
2023-04-21 09:26:34 +0900

    template method before 추가

---
+++ src/main/java/me/kktrkkt/designpattern/templatemethod/Client.java
@@ -0,0 +1,10 @@
+package me.kktrkkt.designpattern.templatemethod;
+
+public class Client {
+
+    public static void main(String[] args) {
+        FileProcessor fileProcessor = new FileProcessor("number.txt");
+        int result = fileProcessor.process();
+        System.out.println(result);
+    }
+}
