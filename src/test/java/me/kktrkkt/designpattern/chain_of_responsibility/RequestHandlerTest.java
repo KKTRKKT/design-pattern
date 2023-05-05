9ab3d37ca286205beb3a971636eababb7dfa7a42
KKTRKKT <paulhana6006@gmail.com>
2023-05-05 13:54:32 +0900

    chain_of_responsibility test 추가

--- src/test/java/me/kktrkkt/designpattern/chain_of_responsibility/RequestHandlerTest.java
+++ src/test/java/me/kktrkkt/designpattern/chain_of_responsibility/RequestHandlerTest.java
@@ -1,10 +1,16 @@
 package me.kktrkkt.designpattern.chain_of_responsibility;
 
-public class Client {
+import org.junit.jupiter.api.Test;
 
-    public static void main(String[] args) {
+import static org.junit.jupiter.api.Assertions.*;
+
+class RequestHandlerTest {
+
+    @Test
+    public void handler_test() {
         Request request = new Request("무궁화 꽃이 피었습니다.");
         RequestHandler requestHandler = new AuthRequestHandler(new LoggingRequestHandler(new DefaultRequestHandler(null)));
         requestHandler.handler(request);
     }
+
 }
